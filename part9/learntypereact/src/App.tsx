8037335dc5d83adb4c221c68287d67bfc232ba35
swu <shuangwumetal@gmail.com>
2024-07-11 03:53:10 +0300

    9.15

--- part9/learntypereact/src/App.tsx
+++ part9/learntypereact/src/App.tsx
@@ -1,19 +1,73 @@
 
 
+
+
 const App = () => { 
   const courseName = "Half Stack application development";
-  const courseParts = [
+
+  interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+  
+  }
+  
+  interface CourseDes extends CoursePartBase {
+    description: string;
+  }
+  
+  interface CoursePartBasic extends CourseDes {
+  
+    kind: "basic"
+  }
+  
+  interface CoursePartGroup extends CoursePartBase {
+    groupProjectCount: number;
+    kind: "group"
+  }
+  
+  interface CoursePartBackground extends CourseDes {
+  
+    backgroundMaterial: string;
+    kind: "background"
+  }
+  interface CoursePartReq extends CourseDes {
+    requirements: string[];
+    kind: "special"
+  }
+  
+  type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartReq;
+  const courseParts : CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group"
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background"
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special"
     }
   ];
 
@@ -26,17 +80,68 @@ const App = () => {
     exerciseCount: number;
   }
 
-  const Content = ({ content }: { content: ContentType[] }): JSX.Element => {
+  /*const Content = ({ content }: { content: ContentType[] }): JSX.Element => {
     return (
       <div>
        {content.map( content => <p key={content.name}>{content.name} {content.exerciseCount}</p>)}
       </div>
     )
+  }*/
+
+  const Part =( {part} : {part: CoursePart}) => { 
+    switch (part.kind) {
+      case "basic":
+        return (
+          <div>
+            <h3>{part.name} {part.exerciseCount}</h3>
+            <p>{part.description}</p>
+          </div>
+        );
+      case "background":
+        return (
+          <div>
+            <h3>{part.name} {part.exerciseCount}</h3>
+            <p>{part.description}</p>
+            <p>submit to  {part.backgroundMaterial}</p>
+          </div>
+        );
+      case "group":
+        return (
+          <div>
+            <h3>{part.name} {part.exerciseCount}</h3>
+            <p>group projects {part.groupProjectCount}</p>
+          </div>
+        );
+      case "special":
+        return (
+          <div>
+            <h3>{part.name} {part.exerciseCount}</h3>
+            <p>{part.description}</p>
+            <p>requirements: {part.requirements.join (" ,")}</p>
+          </div>
+        )
+    
+    }
+  }
+  const Content =({p} : {p : CoursePart []}) => {
+    return (
+      <div>
+        {p.map(part => {
+          return (
+            <div key={part.name}>
+              <Part part={part}/>
+            </div>
+          )
+        })}
+      </div>
+    )
+
   }
 
   const Total = ({ content }: { content: ContentType[] }): JSX.Element => {
     return (
       <div>
+        <br />
         The total amount is:
         {content.reduce((sum, part) => sum + part.exerciseCount, 0)}
       </div>
@@ -48,7 +153,7 @@ const App = () => {
   return (
     <div>
       <Header name={courseName} />
-      <Content content={courseParts}/>
+      <Content p={courseParts}/>
       <Total content = {courseParts} />
     </div>
   );
